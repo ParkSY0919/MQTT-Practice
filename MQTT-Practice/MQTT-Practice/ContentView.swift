28d139c428efaadd3b1fc99bc0f531010e9b025b
ParkSY0919 <qhr498@gmail.com>
2025-09-03 18:35:23 +0900

    [Setting] init Project

---
+++ MQTT-Practice/MQTT-Practice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MQTT-Practice
+//
+//  Created by 박신영 on 9/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MQTT-Practice/MQTT-Practice/MQTT_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MQTT_PracticeApp.swift
+//  MQTT-Practice
+//
+//  Created by 박신영 on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct MQTT_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
